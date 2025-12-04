4577aa8d3d6baf16c8d6d5a3680995883210c900
Nova <noah.a.vito@gmail.com>
2025-12-03 19:27:41 -0500

    more playwright workers

--- nova-webgames-fe/playwright.config.ts
+++ nova-webgames-fe/playwright.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry flaky tests */
   retries: process.env.CI ? 2 : 1,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Use 4 workers in CI for faster test execution */
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
